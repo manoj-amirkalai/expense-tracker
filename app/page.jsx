1ec6cd03a1b0d05008ce9e9a226162a2c5020fb3
Manoj.A <manojamir2911@gmail.com>
2024-09-10 20:59:44 +0530

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,29 +1,471 @@
 "use client";
-import Navbar from "./Components/Navbar/Navbar";
 import styles from "./page.module.css";
 import { transactions } from "./data";
 import { FaWallet } from "react-icons/fa6";
 import { BsGraphDownArrow } from "react-icons/bs";
 import { BsGraphUpArrow } from "react-icons/bs";
+import ApexCharts from "react-apexcharts";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   // console.log("height:", window.innerHeight);
   // console.log("width:", window.innerWidth);
-  const expenseTotal = transactions
-    .filter((transaction) => transaction.type.toLowerCase() === "expense")
-    .map((transaction) => parseFloat(transaction.amount))
+  const [getyear, setgetyear] = useState(2024);
+  const [year, setYear] = useState(2023);
+  const [janExp, setJanExp] = useState(0);
+  const [febEXP, setfebEXP] = useState(0);
+  const [marExp, setmarExp] = useState(0);
+  const [aprExp, setaprExp] = useState(0);
+  const [mayExp, setmayExp] = useState(0);
+  const [junExp, setjunExp] = useState(0);
+  const [julExp, setjulExp] = useState(0);
+  const [augExp, setaugExp] = useState(0);
+  const [sepExp, setsepExp] = useState(0);
+  const [octExp, setoctExp] = useState(0);
+  const [novExp, setnovExp] = useState(0);
+  const [decExp, setdecExp] = useState(0);
+  const [janInc, setjanInc] = useState(0);
+  const [febInc, setfebInc] = useState(0);
+  const [marInc, setmarInc] = useState(0);
+  const [aprInc, setaprInc] = useState(0);
+  const [mayInc, setmayInc] = useState(0);
+  const [junInc, setjunInc] = useState(0);
+  const [julInc, setjulInc] = useState(0);
+  const [augInc, setaugInc] = useState(0);
+  const [sepInc, setsepInc] = useState(0);
+  const [octInc, setoctInc] = useState(0);
+  const [novInc, setnovInc] = useState(0);
+  const [decInc, setdecInc] = useState(0);
+
+  const [transactionsFiltered, setTransactionsFiltered] = useState([
+    ...transactions,
+  ]);
+
+  useEffect(() => {
+    const filteredyear = transactionsFiltered.filter((transaction) => {
+      const yearFilter = new Date(transaction.date).getFullYear();
+
+      return yearFilter === year;
+    });
+    setTransactionsFiltered(filteredyear);
+  }, [year]);
+
+  const calculateJan = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 1;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setjanInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 1;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setJanExp(expense);
+  };
+
+  const calculateFeb = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 2;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setfebInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 2;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setfebEXP(expense);
+  };
+
+  const calculatemar = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 3;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setmarInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 3;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setmarExp(expense);
+  };
+
+  const calculateapr = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 4;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setaprInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 4;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setaprExp(expense);
+  };
+  const calculatemay = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 5;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setmayInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 5;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setmayExp(expense);
+  };
+  const calculatejun = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 6;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setjunInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 6;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setjunExp(expense);
+  };
+  const calculatejul = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 7;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setjulInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 7;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setjulExp(expense);
+  };
+  const calculateaug = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 8;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setaugInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 8;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setaugExp(expense);
+  };
+  const calculatesep = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 9;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setsepInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 9;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setsepExp(expense);
+  };
+  const calculateoct = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 10;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setoctInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 10;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setoctExp(expense);
+  };
+  const calculatenov = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 11;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setnovInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 11;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setnovExp(expense);
+  };
+  const calculatedec = () => {
+    const income = transactionsFiltered
+      .filter((tx) => tx.type === "Income")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 12;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setdecInc(income);
+
+    const expense = transactionsFiltered
+      .filter((tx) => tx.type === "Expense")
+      .filter((tx) => {
+        const month = new Date(tx.date).getMonth() + 1;
+        return month === 12;
+      })
+      .reduce((total, tx) => {
+        return total + parseFloat(tx.amount);
+      }, 0);
+    setdecExp(expense);
+  };
+
+  useEffect(() => {
+    calculateJan();
+    calculateFeb();
+    calculatemar();
+    calculateapr();
+    calculatemay();
+    calculatejun();
+    calculatejul();
+    calculateaug();
+    calculatesep();
+    calculateoct();
+    calculatenov();
+    calculatedec();
+  }, [transactionsFiltered]);
+  const data = {
+    series: [
+      {
+        name: "Expense",
+        data: [
+          janExp / 1000,
+          febEXP / 1000,
+          marExp / 1000,
+          aprExp / 1000,
+          mayExp / 1000,
+          junExp / 1000,
+          julExp / 1000,
+          augExp / 1000,
+          sepExp / 1000,
+          octExp / 1000,
+          novExp / 1000,
+          decExp / 1000,
+        ],
+      },
+      {
+        name: "Income",
+        data: [
+          janInc / 1000,
+          febInc / 1000,
+          marInc / 1000,
+          aprInc / 1000,
+          mayInc / 1000,
+          junInc / 1000,
+          julInc / 1000,
+          augInc / 1000,
+          sepInc / 1000,
+          octInc / 1000,
+          novInc / 1000,
+          decInc / 1000,
+        ],
+      },
+    ],
+    options: {
+      chart: {
+        type: "bar",
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: "85%",
+          endingShape: "rounded",
+        },
+      },
+      dataLabels: {
+        enabled: true,
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ["transparent"],
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+        colors: ["#9A4BFF", "#FF489B", "#FF489B", "#FF489B"],
+      },
+      yaxis: {
+        title: {
+          text: "",
+        },
+      },
+      fill: {
+        opacity: 1,
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return "Rs." + val + "k";
+          },
+        },
+      },
+    },
+  };
+  const expenseTotal = transactionsFiltered
+    .filter(
+      (transactionsFiltered) =>
+        transactionsFiltered.type.toLowerCase() === "expense"
+    )
+    .map((transactionsFiltered) => parseFloat(transactionsFiltered.amount))
     .reduce((total, amount) => total + amount, 0);
 
-  const incomeTotal = transactions
-    .filter((transaction) => transaction.type.toLowerCase() === "income")
-    .map((transaction) => parseFloat(transaction.amount))
+  const incomeTotal = transactionsFiltered
+    .filter(
+      (transactionsFiltered) =>
+        transactionsFiltered.type.toLowerCase() === "income"
+    )
+    .map((transactionsFiltered) => parseFloat(transactionsFiltered.amount))
     .reduce((total, amount) => total + amount, 0);
   const balance = incomeTotal - expenseTotal;
+
   return (
     <div className={styles.page}>
       <div className={styles.dashboard}>
-        <div className={styles.dashboard_chart}></div>
+        <div className={styles.dashboard_chart}>
+          <ApexCharts
+            className={styles.chart}
+            options={data.options}
+            series={data.series}
+            type="bar"
+            height={800}
+          />
+        </div>
         <div className={styles.dashboard_expinc}>
+        <input type="number" onChange={(e)=>setgetyear(e.target.value)} />
+        <button onClick={()=>{setYear(parseFloat(getyear))}}>submit</button>
           <div className={styles.dashboard_balance}>
             <span>
               <FaWallet />
--- app/report/page.jsx
+++ app/report/page.jsx
@@ -1,49 +1,218 @@
-import React from "react";
-import "./page.css";
-import { transactions } from "@/app/data";
-import { RiDeleteBin2Fill } from "react-icons/ri";
-import { LuEye } from "react-icons/lu";
-import { MdEdit } from "react-icons/md";
-const page = () => {
-  return (
-    <div className="expense">
-      {" "}
-      <table>
-        <thead>
-          <tr>
-            <td className="title">Paid For</td>
-            <td className="title">Paid By</td>
-            <td className="title">Paid Using</td>
-            <td className="title">Category</td>
-            <td className="title">Date</td>
-            <td className="title">Time</td>
-            <td className="title">Amount</td>
-            <td className="title">Action</td>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.map((data, index) => {
-            return (
-              <tr key={index} className="">
-                <td>{data.title}</td>
-                <td>{data.paidby}</td>
-                <td>{data.paidusing}</td>
-                <td>{data.type}</td>
-                <td>{data.date}</td>
-                <td>{data.time}</td>
-                <td>{data.amount}</td>{" "}
-                <td className="action">
-                  <LuEye className="action_icons" />&nbsp;&nbsp;
-                  <MdEdit className="action_icons" />&nbsp;&nbsp;
-                  <RiDeleteBin2Fill className="action_icons"  />
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+"use client";
+// import React from "react";
+// import "./page.css";
+// import { transactions } from "@/app/data";
+// import { RiDeleteBin2Fill } from "react-icons/ri";
+// import { LuEye } from "react-icons/lu";
+// import { MdEdit } from "react-icons/md";
+// const page = () => {
+//   return (
+//     <div className="expense">
+//       {" "}
+//       <table>
+//         <thead>
+//           <tr>
+//             <td className="title">Paid For</td>
+//             <td className="title">Paid By</td>
+//             <td className="title">Paid Using</td>
+//             <td className="title">Category</td>
+//             <td className="title">Date</td>
+//             <td className="title">Time</td>
+//             <td className="title">Amount</td>
+//             <td className="title">Action</td>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {transactions.map((data, index) => {
+//             return (
+//               <tr key={index} className="">
+//                 <td>{data.title}</td>
+//                 <td>{data.paidby}</td>
+//                 <td>{data.paidusing}</td>
+//                 <td>{data.type}</td>
+//                 <td>{data.date}</td>
+//                 <td>{data.time}</td>
+//                 <td>{data.amount}</td>{" "}
+//                 <td className="action">
+//                   <LuEye className="action_icons" />&nbsp;&nbsp;
+//                   <MdEdit className="action_icons" />&nbsp;&nbsp;
+//                   <RiDeleteBin2Fill className="action_icons"  />
+//                 </td>
+//               </tr>
+//             );
+//           })}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
 
-export default page;
+// export default page;
+import React, { useRef, useState } from "react";
+import { SearchOutlined } from "@ant-design/icons";
+import { Button, Input, Space, Table } from "antd";
+import Highlighter from "react-highlight-words";
+const data = [
+  { key: "1", name: "John Brown", age: 32, address: "New York No. 1 Lake Park" },
+  { key: "2", name: "Jane Smith", age: 29, address: "London No. 2 Park Lane" },
+  { key: "3", name: "Paul Johnson", age: 45, address: "Paris No. 3 Champs-Elysees" },
+  { key: "4", name: "Emily Davis", age: 37, address: "Berlin No. 4 Alexanderplatz" },
+  { key: "5", name: "Michael Wilson", age: 41, address: "Madrid No. 5 Gran Via" },
+  { key: "6", name: "Sarah Miller", age: 28, address: "Rome No. 6 Via del Corso" },
+  { key: "7", name: "David Lee", age: 35, address: "Tokyo No. 7 Shibuya" },
+  { key: "8", name: "Linda Harris", age: 33, address: "Sydney No. 8 Bondi Beach" },
+  { key: "9", name: "James Clark", age: 40, address: "Toronto No. 9 Queen Street" },
+  { key: "10", name: "Laura Lewis", age: 31, address: "Los Angeles No. 10 Sunset Boulevard" },
+  { key: "11", name: "Robert Walker", age: 39, address: "San Francisco No. 11 Golden Gate" },
+  { key: "12", name: "Jessica Robinson", age: 26, address: "Chicago No. 12 Michigan Avenue" },
+  { key: "13", name: "William Wright", age: 42, address: "Boston No. 13 Back Bay" },
+  { key: "14", name: "Elizabeth Walker", age: 30, address: "Seattle No. 14 Pike Place Market" },
+  { key: "15", name: "Daniel Hall", age: 38, address: "Houston No. 15 Main Street" },
+  { key: "16", name: "Sophia Young", age: 27, address: "Miami No. 16 Ocean Drive" },
+  { key: "17", name: "Matthew Allen", age: 34, address: "Dallas No. 17 Deep Ellum" },
+  { key: "18", name: "Olivia King", age: 43, address: "Atlanta No. 18 Peachtree Street" },
+  { key: "19", name: "Ethan Scott", age: 46, address: "Denver No. 19 16th Street Mall" },
+  { key: "20", name: "Ava Adams", age: 25, address: "Phoenix No. 20 Camelback Mountain" },
+];
+const Page = () => {
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1677ff" : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: "#ffc069",
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      width: "30%",
+      ...getColumnSearchProps("name"),
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+      width: "20%",
+      ...getColumnSearchProps("age"),
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+      ...getColumnSearchProps("address"),
+      sorter: (a, b) => a.address.length - b.address.length,
+      sortDirections: ["descend", "ascend"],
+    },
+  ];
+  return <Table columns={columns} dataSource={data} />;
+};
+export default Page;
